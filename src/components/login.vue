f447a4b1629309c8b16f25cfea658732f8778108
JasonLiuxxx <384804214@qq.com>
2019-05-06 10:58:58 +0800

    增加login组件

---
+++ src/components/login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        登录
+    </div>
+</template>
+
+<script>
+export default {
+name:'login'
+}
+</script>
+
+<style lang="scss" >
+    html,body{
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        .login{
+            height: 100%;
+            background-color: #324152;
+        }
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -3,8 +3,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-const routes = [
+import login from './components/login.vue'
 
+const routes = [
+    {
+        path:'/login',
+        component:login
+    }
 ]
 
 const router = new VueRouter({
