ca2e87816c90053225e09dce7c92759867c25697
JasonLiuxxx <384804214@qq.com>
2019-05-08 14:33:55 +0800

    登录请求

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="login">
     <div class="content-box">
-        <h3 class="title" >用户登录</h3>
-      <el-form :model='loginForm' :rules="loginRules" ref='loginForm' label-position="top" label-width="80px">
-        <el-form-item label="用户名" prop='username' >
-          <el-input v-model='loginForm.username' ></el-input>
+      <h3 class="title">用户登录</h3>
+      <el-form
+        :model="loginForm"
+        :rules="loginRules"
+        ref="loginForm"
+        label-position="top"
+        label-width="80px"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="loginForm.username"></el-input>
         </el-form-item>
-        <el-form-item label="密码" prop='password' >
-          <el-input v-model='loginForm.password'></el-input>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="loginForm.password" type="password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button  class="login-btn" type="primary" @click="submitForm('loginForm')" >登录</el-button>
-          <el-button  class="reset-btn" type="primary" @click="resetForm('loginForm')" >重置</el-button>
+          <el-button class="login-btn" type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button class="reset-btn" type="primary" @click="resetForm('loginForm')">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -21,40 +27,49 @@
 <script>
 export default {
   name: "login",
-  data(){
+  data() {
     return {
-      loginForm:{
-        username:'',
-        password:''
+      loginForm: {
+        username: "",
+        password: ""
       },
       loginRules: {
-          username: [
-            { required: true, message: '请输入用户名', trigger: 'blur' },
-            { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' }
-          ],
-          password: [
-            { required: true, message: '请输入密码', trigger: 'blur' },
-            { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
-          ],
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 5, max: 10, message: "长度在 5 到 10 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6 到 12 个字符", trigger: "blur" }
+        ]
       }
-    }
+    };
   },
 
-   methods: {
-      submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
-   }
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$request.login(this.loginForm).then(res => {
+            if (res.data.meta.status === 400) {
+              this.$message.error(res.data.meta.msg);
+            } else {
+              this.$router.push("/");
+            }
+          });
+        } else {
+          this.$message({
+            message: "输入错误,请重新输入",
+            type: "warning"
+          });
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
 };
 </script>
 
@@ -79,21 +94,22 @@ body {
   border-radius: 10px;
   padding: 50px 35px;
   box-sizing: border-box;
-  .login-btn,.reset-btn{
-      width: 48%;
+  .login-btn,
+  .reset-btn {
+    width: 48%;
   }
-  .title{
-      margin: 0;
-      padding: 0;
-      margin-bottom: 26px;
+  .title {
+    margin: 0;
+    padding: 0;
+    margin-bottom: 26px;
   }
-  .el-form-item__label{
-      padding: 0;
-      line-height: 30px;
+  .el-form-item__label {
+    padding: 0;
+    line-height: 30px;
   }
-  .el-input__inner{
-      height: 30px;
-      line-height: 30px;
+  .el-input__inner {
+    height: 30px;
+    line-height: 30px;
   }
 }
 </style>
--- src/lib/http.js
+++ src/lib/http.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+
 const request = {
-   
+   login(params){
+       return axios.post('login',params)
+   }
 }
 
 export default {
